db8d7c913d8be11d8146a24d0f6024a0059cc3bb
syed amer khadri <syedamer92@gmail.com>
2022-01-06 12:19:20 -0500

    Files Added

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -6,8 +6,8 @@ var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === "PRODUCTION") {
     baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-    // baseURL = "https://backend-dq-dairyqueen-jithin.herokuapp.com/";
-    baseURL="http://127.0.0.1:8000/";
+     baseURL = "https://ecommerce-backend-restaurant.herokuapp.com/";
+    // baseURL="http://127.0.0.1:8000/";
 }
 
 const api = axios.create({
